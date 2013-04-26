c33aa4f21ec1fb07b0c8b82e386dd11ec7598ed4
Lori J <mintblue87@gmail.com>
2013-04-26 13:14:19 -0700

    Make search look nice, fix problem with tissue validation

--- app/models/tissue.rb
+++ app/models/tissue.rb
@@ -1,8 +1,12 @@
 class Tissue < ActiveRecord::Base
 #  attr_accessible :name, :total
   validates :name, :total, presence: true
+  validates :name, uniqueness: true
   validates :total, numericality: true
   
   has_many :values # LJ
+
+
+  #  scope :rotting, -> { where(rotting: true) } 
 
 end
