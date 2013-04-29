0b9c72f1ac5d92e56eada0f5ebf80d432e784e11
Lori J <mintblue87@gmail.com>
2013-04-29 11:46:23 -0700

    cleaning up views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Genes::Application.routes.draw do
 
   get "genes/index"
 
-  get "home/index"
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -67,7 +67,7 @@ Genes::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'home#index'
+   root :to => 'genes#index'
 
   # See how all your routes lay out with "rake routes"
 
