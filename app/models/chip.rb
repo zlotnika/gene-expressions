1c260f0bfeda513eb5e314c85e3fe8f96d8464a0
Lori J <mintblue87@gmail.com>
2013-04-29 23:26:54 -0700

    making so you can see some expressions and stuff

--- app/models/chip.rb
+++ app/models/chip.rb
@@ -7,4 +7,15 @@ class Chip < ActiveRecord::Base
   belongs_to :gene
   has_many :expressions #LJ
 
+  def find_expression(body_part_id)
+    ex = self.expressions.where("tissue_id = #{body_part_id}").first  # has to have .first, because this otherwise returns an array
+    return ex
+  end
+
+# => Expression(id: integer, mean: float, standard_deviation: float, chip_id: integer, tissue_id: integer, created_at: datetime, updated_at: datetime) 
+#1.9.3-p362 :064 > ex.class()
+# returning:  => ActiveRecord::Relation
+# but I want an Expression object
+
+
 end
