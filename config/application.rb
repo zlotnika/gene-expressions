d34674c3b0648c7ef76beded47e010d441f98417
Lori J <mintblue87@gmail.com>
2013-05-02 11:02:19 -0700

    configuring for heroku

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,9 @@ module Genes
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    #LJ added for heroku
+    config.assets.initialize_on_precompile = false
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
