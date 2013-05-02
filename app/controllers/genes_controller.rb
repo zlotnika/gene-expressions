66399429e6db55b14c31ea8ca41a295e016c5d4d
Lori J <mintblue87@gmail.com>
2013-05-02 12:16:35 -0700

    tidying

--- app/controllers/genes_controller.rb
+++ app/controllers/genes_controller.rb
@@ -6,7 +6,6 @@ class GenesController < ApplicationController
     @chips = Chip.all
     @expressions_relevant = Expression.meaningful
     @found_genes = Gene.search(params[:search])
-#    @gene = Gene.find(params[:id])
   end
 
   def new
@@ -18,15 +17,7 @@ class GenesController < ApplicationController
     @chips = @gene.get_chips
     @expressions = @gene.get_expressions
     @expressions_hash = @gene.get_expressions_hash(@chips) # to_json maybe...
-    @json_gene = @gene.to_json
     @means = @gene.get_means_to_plot
-#    @expressions_hash = {}
- #   @chips.each do |chip|
-  #    x = {}
-   #   ex_array = @gene.get_expressions_by_chip(chip.id)
-    #  x[chip] = @gene.parse_expressions(ex_array)
-    #  @expressions_hash.merge!(x)
-#    end
   end
 
   def update
