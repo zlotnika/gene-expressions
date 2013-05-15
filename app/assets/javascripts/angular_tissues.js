0233efac69af2577c42593d3d5a9ccaec1131c3e
Lori J <mintblue87@gmail.com>
2013-05-15 13:28:16 -0600

    still troubles

--- app/assets/javascripts/angular_tissues.js
+++ app/assets/javascripts/angular_tissues.js
@@ -1,14 +1,14 @@
-var TissuesApp = angular.module('TissuesApp', [])
+var TissuesApp = angular.module('TissuesApp', ['ngResource'])
 
 TissuesApp.controller('TissuesCtrl', ['$scope', function($scope){
     $scope.heading = "hello, world";
+    $scope.things = ["hi","hello"];
     $scope.tissues = [
 	{name: "spleen", total: 11},
 	{name: "elbow"},
         {name: "heart"}];
     $scope.todos = [
-    {text:'learn angular', done:true},
-    {text:'build an angular app', done:false},
+    {text:'learn stuff', done:true},
+    {text:'build a thing', done:false},
     {text:'stuff', done: false}];
-
 }])
