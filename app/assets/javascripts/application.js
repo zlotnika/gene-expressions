c59f2da78fdfcfbcb8d98901fdf9a97bf58d0d88
Alex Zlotnik <zlotnika@gmail.com>
2013-05-15 14:46:42 -0700

    fix weird merger stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require angular.min
+//= require angular-resource.min
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
