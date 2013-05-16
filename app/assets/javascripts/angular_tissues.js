ac1f5c5d132eb9bdb6959e8e24592581bb50953e
Lori J <mintblue87@gmail.com>
2013-05-16 12:36:21 -0600

    attaching database stuff to the angular view

--- app/assets/javascripts/angular_tissues.js
+++ app/assets/javascripts/angular_tissues.js
@@ -1,6 +1,8 @@
 var TissuesApp = angular.module('TissuesApp', ['ngResource'])
 
-TissuesApp.controller('TissuesCtrl', ['$scope', function($scope){
+
+TissuesApp.controller('TissuesCtrl', ['$scope', 'Tissue', function($scope, Tissue){
+    $scope.real_tissues = Tissue.query();
     $scope.heading = "hello, world";
     $scope.things = ["hi","hello"];
     $scope.tissues = [
@@ -8,8 +10,24 @@ TissuesApp.controller('TissuesCtrl', ['$scope', function($scope){
 	{name: "elbow"},
         {name: "heart"}];
     $scope.todos = [
-    {text:'learn stuff', done:true},
-    {text:'build a thing', done:false},
-    {text:'stuff', done: false}];
-    console.log('scope: '+$scope)
+	{text:'learn stuff', done:true},
+	{text:'build a thing', done:false},
+	{text:'stuff', done: false}];
+    
+    $scope.done_todos = function(){
+	done_ones = [];
+	angular.forEach($scope.todos, function(todo){
+	    if (todo.done){
+		done_ones.push(todo);
+	    }
+	})
+	return done_ones;
+    }
+    $scope.finisheds = $scope.done_todos();
 }])
+
+TissuesApp.factory('Tissue', ['$resource', function($resource){
+    console.log($resource('/tissues'))
+    return $resource('/tissues');
+}])
+
--- app/controllers/tissues_controller.rb
+++ app/controllers/tissues_controller.rb
@@ -1,6 +1,12 @@
 class TissuesController < ApplicationController
+  
+  respond_to :json # this is required to make the angular-resource work.  otherwise it doesn't get the Tissues in a way it likes
+
   def index
-    @tissues = Tissue.all
+    respond_to do |format|
+      format.html
+      format.json { render json: Tissue.all }
+    end
   end
 
   def new
