b60f707bc5022fbe1fdff8c1154ead822284fa26
Alex Zlotnik <zlotnika@gmail.com>
2013-05-19 12:16:14 -0700

    got flot to work in directive

--- app/assets/javascripts/angular_tissues.js
+++ app/assets/javascripts/angular_tissues.js
@@ -3,6 +3,10 @@ var TissuesApp = angular.module('TissuesApp', ['ngResource'])
 TissuesApp.controller('TissuesCtrl', ['$scope', 'Tissue', function($scope, Tissue){
     $scope.thing1 = "bindings are fun"
     $scope.tissues = Tissue.query();
+    $scope.change_tissues = function(){
+	$scope.tissues.push({name: 'new_tissue', total: 1})
+    }
+
     $scope.get_tissue_names = function(){
 	data = []
 	for (i = 0; i < $scope.tissues.length; i++){
@@ -13,13 +17,13 @@ TissuesApp.controller('TissuesCtrl', ['$scope', 'Tissue', function($scope, Tissu
 	total = []
 	for(i=0; i < 100; i++){
 	    total.push($scope.tissues[i])
-	  }
-	return total
 	}
+	return total
+    }
     $scope.thing = $scope.return_thing()
     $scope.datas = $scope.get_tissue_names()
     $scope.get_tissue_totals = function(){
-	console.log("getting tissue totals")
+//	console.log("getting tissue totals")
 	totals = []
 	for (i = 0; i < $scope.tissues.length; i++){
 	    console.log($scope.tissues[i].total);
@@ -43,34 +47,34 @@ TissuesApp.controller('TissuesCtrl', ['$scope', 'Tissue', function($scope, Tissu
 	return done_ones;
     }
     $scope.finisheds = $scope.done_todos();
-  $(function(){
-    var data = [$scope.get_tissue_totals()]
-    var data = [[[4, "erstwhile"],[3,"kitten"],[5,$scope.thing1]]];
-    console.log(data)
-    var options = {
-      series: {
-        bars: {
-          fill: true,
-          show: true,
-          barWidth: 0.6,
-          horizontal: true,
-          align: "center" }
-      },
-      yaxis: {
-         mode: "categories",
-         tickLength: 0
-         },
-      xaxis: {
-        position: top
+
+    $(function(){
+	var data = [$scope.get_tissue_totals()]
+	var data = [[[4, "erstwhile"],[3,"kitten"],[5,$scope.thing1]]];
+//	console.log(data)
+	var options = {
+	    series: {
+		bars: {
+		    fill: true,
+		    show: true,
+		    barWidth: 0.6,
+		    horizontal: true,
+		    align: "center" }
+	    },
+	    yaxis: {
+		mode: "categories",
+		tickLength: 0
+            },
+	    xaxis: {
+		position: top
+	    }
 	}
-    }
-    $.plot($("#tissues_chart"), data, options)
-})
+	$.plot($("#tissues_chart"), data, options)
+    })
 
 }])
 
 TissuesApp.factory('Tissue', ['$resource', function($resource){
-    console.log($resource('/tissues'))
     return $resource('/tissues');
 }])
 
@@ -84,5 +88,55 @@ TissuesApp.filter('sort_by_totals', function(){
     }
 })
 
+TissuesApp.directive('chart', function(){
+    return{
+        restrict: 'E',
+        link: function(scope, elem, attrs){
 
-  
+	    var dataize = function(data){
+		return $.map(data,function(tissue){
+		    return [[tissue.name, tissue.total]]
+		});
+	    }
+
+            var chart = null,
+                opts  = { };
+                   
+            scope.$watch(attrs.ngModel, function(v){
+		v = dataize(v)
+		v = [v]
+//		v = [ [[0, 1], [1, 5], [2, 2]] ]
+		console.log(v)
+
+	var opts = {
+	    series: {
+		bars: {
+		    fill: true,
+		    show: true,
+		    barWidth: 0.6,
+//		    horizontal: true,
+		    align: "center" }
+	    },
+	    xaxis: {
+		mode: "categories",
+		tickLength: 0
+            },
+	    yaxis: {
+		position: top
+	    }
+	}
+
+
+                if(!chart){
+                    chart = $.plot(elem, v , opts);
+                    $(elem).show();
+                }else{
+                    chart.setData(v);
+                    chart.setupGrid();
+                    chart.draw();
+                }
+            }, true)
+	    
+        }
+    };
+});
